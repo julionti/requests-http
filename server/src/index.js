e76428e8e3bea07c25cc4ec84d09a886fdfff09b
HuLius <julionti@gmail.com>
2020-10-02 10:18:12 -0300

    Http: Removendo CORS com Angular Proxy

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const cord = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const multipart = require('connect-multiparty');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: '*', // Origem da requisiçao
-  optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//   origin: '*', // Origem da requisiçao
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
-app.use(cord(corsOptions));
+const multipartMiddleware = multipart({ uploadDir: './uploads' });
 
-const multipartMiddleware = multipart({ uploadDir: './uploads'});
-app.post('upload', multipartMiddleware, (req, res) => {
+app.post('/upload', multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
-  res.json({ message: files});
+  res.json({ message: files });
 });
 
 app.use((err, req, res, next) => res.json({error: err.message}));
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files: Set<File>; // Set Não permite arquivos duplicados
+  serv;
+
+  constructor(private service: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -19,11 +24,25 @@ export class UploadFileComponent implements OnInit {
     const selectedFiles = event.srcElement.files as FileList;
     // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
 
-    //para vários arquivos
+    // para vários arquivos
     const fileNames = [];
+    this.files = new Set();
     for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.serv = this.service.upload(this.files, environment.BASE_URL + '/upload') // Tudo q for /api não vai ser rota mas sim chamada para o backend
+        .subscribe(response => console.log('Upload Concluído!'));
+
+    }
+  }
+
+  OnDestroy() {
+    this.serv.unsubscribe();
   }
 }
